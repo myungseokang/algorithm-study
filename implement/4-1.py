051898c7514b9224b4c2dd70aa094b4b17a5d744
Myungseo Kang <l3opold7@gmail.com>
2021-01-20 21:33:41 +0900

    x, y 좌표 수정

--- implement/4-1.py
+++ implement/4-1.py
@@ -4,13 +4,13 @@ character = [1, 1]
 move_plan = input().split(' ')
 
 for plan in move_plan:
-    if plan == 'R' and character[1] + 1 <= N:
-        character[1] += 1
-    elif plan == 'L' and character[1] - 1 >= N:
-        character[1] -= 1
-    elif plan == 'U' and character[1] - 1 >= N:
-        character[0] -= 1
-    elif plan == 'D' and character[1] + 1 <= N:
+    if plan == 'R' and character[0] + 1 <= N:
         character[0] += 1
+    elif plan == 'L' and character[0] - 1 >= N:
+        character[0] -= 1
+    elif plan == 'U' and character[1] - 1 >= N:
+        character[1] -= 1
+    elif plan == 'D' and character[1] + 1 <= N:
+        character[1] += 1
 
 print(f"{character[0]} {character[1]}")
