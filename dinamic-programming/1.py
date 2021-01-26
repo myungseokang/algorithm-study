baa24153563674ef40ef4874aaf49caebfe2e179
Myungseo Kang <l3opold7@gmail.com>
2021-01-26 21:07:56 +0900

    dp

--- dinamic-programming/1.py
+++ dinamic-programming/1.py
@@ -10,13 +10,13 @@ for i in range(2, N + 1):
     calc_map[i] = calc_map[idx] + 1
 
     if i % 2 == 0:
-        calc_map[i] = min(calc_map[i], calc_map[idx // 5] + 1)
+        calc_map[i] = min(calc_map[i], calc_map[idx // 2] + 1)
 
     if i % 3 == 0:
         calc_map[i] = min(calc_map[i], calc_map[idx // 3] + 1)
 
     if i % 5 == 0:
-        calc_map[i] = min(calc_map[i], calc_map[idx // 2] + 1)
+        calc_map[i] = min(calc_map[i], calc_map[idx // 5] + 1)
 
 print(calc_map)
 print(calc_map[N])
